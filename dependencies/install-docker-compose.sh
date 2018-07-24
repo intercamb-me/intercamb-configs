2a77bdc56b4292736632231b6cb5d35011029f2c
Sandro Simas <sandro.csimas@gmail.com>
2018-07-24 19:26:44 -0300

    Upgrading docker compose version;

--- dependencies/install-docker-compose.sh
+++ dependencies/install-docker-compose.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Installing Docker Compose..."
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Docker Compose installed with success!"
