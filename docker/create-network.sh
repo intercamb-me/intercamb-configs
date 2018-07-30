80d158187be9c1cff3bd6647f6b6bd5012bad9e6
Sandro Simas <sandro.csimas@gmail.com>
2018-07-30 20:59:14 -0300

    Changing network ip

--- docker/create-network.sh
+++ docker/create-network.sh
@@ -6,9 +6,9 @@ echo "Creating Intercambio network on Docker..."
 
 docker network create \
   --driver bridge \
-  --subnet 172.28.0.0/16 \
-  --ip-range 172.28.5.0/24 \
-  --gateway 172.28.5.254 \
+  --subnet 172.30.0.0/16 \
+  --ip-range 172.30.5.0/24 \
+  --gateway 172.30.5.254 \
   intercambio-network
 
 echo "Intercambio network was created with success!"
